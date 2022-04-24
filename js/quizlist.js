a3c3b30ae1d5cb220cad1b0f95e4c03e7eaccd13
Henrik Stiernstedt <henrik@stiernstedt.net>
2022-04-24 19:29:04 +0200

    An example button from the turorial

--- js/quizlist.js
+++ js/quizlist.js
@@ -467,3 +467,12 @@ function initQuizlist() {
   });
 
 }
+
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
